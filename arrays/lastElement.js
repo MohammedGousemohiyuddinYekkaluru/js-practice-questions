9dcca6f2d772d71169bd8f098179fe067fc59f52
mohammed gouse <gousemohammed163@gmail.com>
2025-11-18 09:59:28 +0530

    Add last Element

--- arrays/lastElement.js
+++ arrays/lastElement.js
@@ -8,3 +8,13 @@ function getLastElement(arr){
 
 let lastElement = getLastElement(arr);
 console.log(lastElement)
+
+//another method
+
+function getLastElement1(arr) {
+    let newArr = [...arr]
+    return newArr.pop()
+}
+
+const lastElement1 = getLastElement1(arr)
+console.log(lastElement1)
