d77a0eb7f5261c738d154f5ec1b42e55c6d4b19b
mohammed gouse <gousemohammed163@gmail.com>
2025-11-21 08:46:34 +0530

    Remove duplicates from an array

--- arrays/uniqueValues.js
+++ arrays/uniqueValues.js
@@ -5,3 +5,16 @@ function uniqueValues(arr1, arr2){
 }
 
 console.log(uniqueValues([1, 2, 3], [3, 4, 5]));
+
+
+//another way to remove duplicate values
+
+function removeDuplicates(arr) {
+    let uniqueArr = arr.filter((element, index) => {
+        return arr.indexOf(element) === index
+    })
+
+    return uniqueArr
+}
+
+console.log(removeDuplicates([1, 5, 3, 1, 1, 1, 3]))
