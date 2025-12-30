6921d8fe263af7905147b7edcb317f0755cf5d07
mohammed gouse <gousemohammed163@gmail.com>
2025-12-30 16:53:57 +0530

    Solve Add only numbers

---
+++ mixed/AddOnlyNumbers.js
@@ -0,0 +1,12 @@
+// In an array of numbers and strings, only add those numbers which are not strings
+
+let arr = [1, "a", "2", "b", 3, 4, "5", 6, "c"];
+
+function addOnlyNumbers(arr) {
+  return arr.reduce((sum, item) => 
+    (typeof item === "number" ? sum + item : sum),
+    0);
+}
+
+sumOfNumbers = addOnlyNumbers(arr);
+console.log(sumOfNumbers); 
