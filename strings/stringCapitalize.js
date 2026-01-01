7847de01212bae939a73837cd13e471fb68778ac
mohammed gouse <gousemohammed163@gmail.com>
2026-01-01 14:21:35 +0530

    Add another function to capitalize multiple words in one string

--- strings/stringCapitalize.js
+++ strings/stringCapitalize.js
@@ -4,4 +4,13 @@ function strCapitalize(str) {
     return str ? str.charAt(0).toUpperCase() + str.slice(1) : "";
 }
 
-console.log(strCapitalize("javascript"));
+// console.log(strCapitalize("javascript"));
+
+// OR
+
+function strCapitalize2(str) {
+    return str.split(" ").map(item => 
+        item.charAt(0).toUpperCase() + item.slice(1)
+    ).join(" ")
+}
+console.log(strCapitalize2("mohammed gouse mohiyuddin"))
