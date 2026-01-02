8e5b3d13b306fedeaa765d74e99dc29d8b70297e
mohammed gouse <gousemohammed163@gmail.com>
2026-01-02 21:32:15 +0530

    Fix: the function to show the most repeated items

--- arrays/mostFreqItem.js
+++ arrays/mostFreqItem.js
@@ -11,7 +11,13 @@ function mostFreqItem(arr) {
         }
     })
 
-    return Math.max(...Object.values(freqObj))
+    let maxFreq =  Math.max(...Object.values(freqObj))
+
+    let keyValueArr = Object.entries(freqObj)
+    let resultArr = keyValueArr.filter(arr => {
+        return arr[1] === maxFreq
+    })
+    return resultArr
 }
 
-console.log(mostFreqItem([1, 2, 2, 3, 4, 4, 4, 5]))
+console.log(mostFreqItem([1, 2, 2, 2, 3, 4, 4, 4, 5]))
