29d940d46f69347b7d65f850cfac46f49d0d63d6
mohammed gouse <gousemohammed163@gmail.com>
2026-01-03 16:08:47 +0530

    Add shuffling the array

---
+++ arrays/shuffle.js
@@ -0,0 +1,12 @@
+// Write a js function to shuffle an array
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let idx in arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  let temp = arr[idx];
+  arr[idx] = arr[randomIndex];
+  arr[randomIndex] = temp;
+}
+
+console.log(arr);
