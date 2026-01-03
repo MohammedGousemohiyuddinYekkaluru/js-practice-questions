b83d73938c347d01e624f4663984ba7b70dc1ff4
mohammed gouse <gousemohammed163@gmail.com>
2026-01-03 16:53:39 +0530

    Add DOM update based on the user input

--- DOM/01_update_in_realTime/script.js
+++ DOM/01_update_in_realTime/script.js
@@ -1,5 +1,15 @@
 // Update the DOM based on the user input in real-time
 
-let name = document.querySelector(".name")
-let email = document.querySelector(".email")
+let nameInput = document.querySelector("#name")
+let emailInput = document.querySelector("#email")
+let nameVal = document.querySelector(".name")
+let emailVal = document.querySelector(".email")
 
+
+nameInput.addEventListener("input", (e) => {
+    nameVal.innerText = e.target.value
+})
+
+emailInput.addEventListener("input", (e) => {
+    emailVal.innerText = e.target.value
+})
