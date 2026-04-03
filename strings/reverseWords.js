49dbbfb82d8930d278cdfc6c1faa6865f34c5a18
mohammed gouse <gousemohammed163@gmail.com>
2026-04-03 14:57:14 +0530

    Given a string reverse each word in the sentence

--- strings/reverseWords.js
+++ strings/reverseWords.js
@@ -1,3 +1,18 @@
+// Given a string reverse each word in the sentence
+
+let str = "Hello! My name is Gouse";
+
+let reverseWordString = str.split(" ").map(function(word){
+    return word.split("").reverse().join("")
+}).join(" ");
+
+console.log(reverseWordString);
+
+
+
+
+
+
 //Write a function that reverses each word individually, not the whole sentence.
 
 function reverseWords(sentence){
