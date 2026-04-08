0387184dc71b9339a6f1dc8314622e964c12684d
mohammed gouse <gousemohammed163@gmail.com>
2026-04-08 11:29:20 +0530

    create a function that takes an array containing both numbers and strings, and returns a new array containing only the string values

--- arrays/getStringValues.js
+++ arrays/getStringValues.js
@@ -1,5 +1,22 @@
 // create a function that takes an array containing both numbers and strings, and returns a new array containing only the string values
 
+function getStringValues(arr) {
+  let stringsOnly = [];
+  
+  for (let val of arr) {
+    if (typeof val === 'string') {
+      stringsOnly.push(val);
+    }
+  }
+  
+  return stringsOnly;
+}
+
+const arr1 = [1, 2, "a", "b", 3, "c"];
+console.log(getStringValues(arr1));
+
+// Another Method
+
 let arr = [1, 2, "a", "b", 3, "c"]
 
 function getStringValues(arr){
