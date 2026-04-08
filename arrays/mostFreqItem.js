807e630c87061b73ca4bfe4d561d5c7c4f750651
mohammed gouse <gousemohammed163@gmail.com>
2026-04-08 11:24:07 +0530

    Write a js program to find the most frequent item of an array

--- arrays/mostFreqItem.js
+++ arrays/mostFreqItem.js
@@ -21,3 +21,25 @@ function mostFreqItem(arr) {
 }
 
 console.log(mostFreqItem([1, 2, 2, 2, 3, 4, 4, 4, 5]))
+
+// Another Method
+
+function getMostFrequent(arr) {
+  const counts = new Map();
+  let maxElement = arr[0];
+  let maxCount = 0;
+
+  for (const item of arr) {
+    const count = (counts.get(item) || 0) + 1;
+    counts.set(item, count);
+
+    if (count > maxCount) {
+      maxCount = count;
+      maxElement = item;
+    }
+  }
+
+  return { item: maxElement, count: maxCount };
+}
+
+console.log(getMostFrequent([1, 2, 2, 2, 3, 4, 4, 4, 5])); 
